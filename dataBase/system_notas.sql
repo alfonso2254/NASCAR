108db4dd6105c89d9c543e2bf9c3ce4563b287fa
alfonso2254 <45520382+alfonso2254@users.noreply.github.com>
2019-05-07 05:43:32 -0400

    Update system_notas.sql

--- dataBase/system_notas.sql
+++ dataBase/system_notas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 07-05-2019 a las 06:33:21
--- Versión del servidor: 5.7.24
--- Versión de PHP: 7.2.14
+-- Servidor: localhost:3306
+-- Tiempo de generación: 07-05-2019 a las 09:42:50
+-- Versión del servidor: 10.3.14-MariaDB
+-- Versión de PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `system_notas`
+-- Base de datos: `id9511978_sysnotes`
 --
 
 -- --------------------------------------------------------
@@ -28,36 +28,23 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `contacto`
 --
 
-DROP TABLE IF EXISTS `contacto`;
-CREATE TABLE IF NOT EXISTS `contacto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `asunto` varchar(100) NOT NULL,
-  `mensaje` varchar(500) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+CREATE TABLE `contacto` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `correo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `asunto` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `mensaje` varchar(1000) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `contacto`
 --
 
 INSERT INTO `contacto` (`id`, `nombre`, `correo`, `asunto`, `mensaje`) VALUES
-(1, 'Luis Alfonso Alarcon', 'alfonso22542254@gmail.com', 'Excelenete', 'Excelente sistema amigo'),
-(2, 'Luis Alfonso Alarcon', 'alfonso22542254@gmail.com', 'Excelenete', 'Excelente sistema amigo'),
-(3, 'Luis Alfonso Alarcon', 'alfonso22542254@gmail.com', 'Excelenete', 'Perfecto'),
-(4, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', '   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo   consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse   cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-(5, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', ''),
-(6, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', ''),
-(7, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', ''),
-(17, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', ''),
-(18, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', ''),
-(19, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', 'a'),
-(20, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', 's'),
-(21, '', '', '', ''),
-(22, '', '', '', ''),
-(23, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', 'pefecto'),
-(24, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', 'loem');
+(1, 'Luis Alfonso Alarcon', 'alfonso22542254@gmail.com', 'Excelenete', 'Excelente sistema Luis Alfonso Alarcon'),
+(2, 'Luis Alfonso Alarcon', 'alfonso22542254@gmail.com', 'Excelenete', 'Perfecto'),
+(3, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', '\r\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+(4, 'Luis Alfonso Alarcon', 'megafox@gmail.com', 'Excelenete', '\r\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
 -- --------------------------------------------------------
 
@@ -65,28 +52,25 @@ INSERT INTO `contacto` (`id`, `nombre`, `correo`, `asunto`, `mensaje`) VALUES
 -- Estructura de tabla para la tabla `notas`
 --
 
-DROP TABLE IF EXISTS `notas`;
-CREATE TABLE IF NOT EXISTS `notas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `notas` (
+  `id` int(11) NOT NULL,
   `titulo` varchar(100) NOT NULL,
   `texto` varchar(5000) NOT NULL,
-  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `usuario_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `usuario_id` (`usuario_id`),
-  KEY `usuario_id_2` (`usuario_id`),
-  KEY `usuario_id_3` (`usuario_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=latin1;
+  `fecha` timestamp NULL DEFAULT current_timestamp(),
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `notas`
 --
 
 INSERT INTO `notas` (`id`, `titulo`, `texto`, `fecha`, `usuario_id`) VALUES
-(66, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2019-05-06 19:59:03', 19),
-(67, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2019-05-06 19:59:07', 19),
-(68, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2019-05-07 02:07:41', 19),
-(69, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2019-05-07 02:07:45', 19);
+(55, 'Monociclos', 'Son rápidos y bonitos. ', '2019-05-05 00:25:07', 20),
+(62, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud.', '2019-05-05 20:58:16', 19),
+(63, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud', '2019-05-05 20:58:22', 19),
+(64, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud', '2019-05-05 20:58:26', 19),
+(65, 'Historia de Venezuela', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud ', '2019-05-05 20:58:30', 19),
+(66, 'Historia de Venezuela', ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2019-05-07 06:24:17', 21);
 
 -- --------------------------------------------------------
 
@@ -94,23 +78,70 @@ INSERT INTO `notas` (`id`, `titulo`, `texto`, `fecha`, `usuario_id`) VALUES
 -- Estructura de tabla para la tabla `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(30) NOT NULL,
   `usuario` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
-  `img` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+  `img` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`id`, `nombre`, `correo`, `usuario`, `password`, `img`) VALUES
-(19, 'Luis Alfonso Alarcon', 'admin@admin.com', 'admin', 'admin', 'perfil_avatar.jpg');
+(19, 'Luis Alfonso Alarcon', 'alfonso22542254@gmail.com', 'alfonso2254', '20121997', '42660444_2083702991693666_7727161123492855808_n.jpg'),
+(20, 'Carlos', 'comglorerate1@gmail.com', 'comglorerate', 'Mnn8Jtt1', '42660444_2083702991693666_7727161123492855808_n.jpg'),
+(21, 'Luis Alberto', 'admin@admin.com', 'admin', 'admin', 'perfil_avatar.jpg');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `contacto`
+--
+ALTER TABLE `contacto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `usuario_id_2` (`usuario_id`),
+  ADD KEY `usuario_id_3` (`usuario_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `contacto`
+--
+ALTER TABLE `contacto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Restricciones para tablas volcadas
