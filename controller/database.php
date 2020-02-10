3b9cc0f7b02b3b15b78b8963a4c06943af2307e5
alfonso2254 <45520382+alfonso2254@users.noreply.github.com>
2020-02-09 20:08:32 -0400

    a

--- controller/database.php
+++ controller/database.php
@@ -1,5 +1,9 @@
 <?php 
-$bd=mysqli_connect('localhost','root','20121997','system_notas');
+$servidor = 'localhost';
+$usuario = 'root';
+$password = '';
+$db = 'system_notas';
+$bd=mysqli_connect($servidor,$usuario,$password,$db);
 
 if (!$bd) {
 	echo "Error al conectarse con la base de datos, por favor comuniquese con el administrador.";
